b6d27e86bee09a1d9d1cf7f8129c025df2f4565e
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-10 13:10:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ def go_to_cafe(friends: list, cafe: str) -> str:
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
 
-    return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {caffe.name}"
